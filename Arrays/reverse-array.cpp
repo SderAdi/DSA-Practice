a10440872d38aaa03a790fb3a8e75472065b0557
Aditya Sharma <adityasharmasder@gmail.com>
2026-03-23 12:48:22 +0530

    Add reverse array solution

---
+++ Arrays/reverse-array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int arr[] {1,2,3,4,5};
+    int sz = sizeof(arr)/sizeof(arr[0];
+    int st = 0, end = sz-1;
+    while (st < end) {
+        swap(arr[st],arr[end]);      
+        st++;
+        end--;
+    }
+    for (int i = 0; i < sz; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
