c9309adeb2a895f5d4cd483a672c882a0f7217e2
Aditya Sharma <adityasharmasder@gmail.com>
2026-03-23 13:16:10 +0530

    Update 03-check-if-sorted.cpp

--- Arrays/03-check-if-sorted.cpp
+++ Arrays/03-check-if-sorted.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main (){
     int arr[] = {1,2,3,4,5};
-    int sz = sizeof(arr)/sizeof(arr[0]);;
+    int sz = sizeof(arr)/sizeof(arr[0]);
     bool sorted = true;
     for (int i = 0; i < sz-1; i++){
         if (arr[i] > arr[i+1]){
